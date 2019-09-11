e1f34acfd0df47e230074fa812c6ab3d6a597e24
Mwamodo Erick <mwamodojnr@gmail.com>
2019-09-11 17:42:00 +0300

    complete

---
+++ components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="flex">
+    <h1 class="text-3xl">Dad Jokes</h1>
+    <ul class="flex">
+      <li>
+        <nuxt-link to="/">Home</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/jokes">Jokes</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/about">About</nuxt-link>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHead"
+};
+</script>
+
+<style scoped>
+</style>
--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,11 +1,70 @@
 <template>
   <div>
     <h1>Jokes</h1>
+    <SearchJokes v-on:search-text="searchText" />
+    <Joke :key="joke.key" v-for="joke in jokes" :id="joke.id" :joke="joke.joke" />
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import Joke from "../../components/joke";
+import SearchJokes from "../../components/SearchJokes";
+
+export default {
+  components: {
+    Joke,
+    SearchJokes
+  },
+  data() {
+    return {
+      jokes: []
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json"
+      }
+    };
+    try {
+      const res = await axios.get("https://icanhazdadjoke.com/search", config);
+      this.jokes = res.data.results;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  head() {
+    return {
+      title: "Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best Place for Dad jokes"
+        }
+      ]
+    };
+  },
+  methods: {
+    async searchText(text) {
+      const config = {
+        headers: {
+          Accept: "application/json"
+        }
+      };
+      try {
+        const res = await axios.get(
+          `https://icanhazdadjoke.com/search?term=${text}`,
+          config
+        );
+        this.jokes = res.data.results;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+};
 </script>
 
 <style>
